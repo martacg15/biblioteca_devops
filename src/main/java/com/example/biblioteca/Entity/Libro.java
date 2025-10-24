2790c0a5c44544c15037c7266c1be029bfe2681f
Marta Cortina Gonzalez <martacg15@educastur.es>
2025-10-24 20:49:51 +0200

    lombok, libro etc

--- src/main/java/com/example/biblioteca/Entity/Libro.java
+++ src/main/java/com/example/biblioteca/Entity/Libro.java
@@ -1,4 +1,22 @@
 package com.example.biblioteca.Entity;
 
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "libro")
 public class Libro {
+    @Id
+    @GeneratedValue(strategy = = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column
+    private String autor;
+
 }
